23c0f96ab568696f9145caba31313f62ec6d2ff7
marina-toledo <marina.mi.toledo@hotmail.com>
2021-03-05 13:32:02 +0100

    adding all test cases given in description of the project

--- src/test/java/com/mmtoledotecnologiadainformacao/formatter/service/FormatterTest.java
+++ src/test/java/com/mmtoledotecnologiadainformacao/formatter/service/FormatterTest.java
@@ -8,8 +8,58 @@ public class FormatterTest {
     Formatter formatter = new Formatter();
 
     @Test
-    public void shouldReturnTextRightAligned() throws Exception {
+    public void shouldReturnTextLeftAlignedWithWidth10() throws Exception {
         String input = "This text should be left aligned ";
+
+        String output = Formatter.format(input);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+
+    @Test
+    public void shouldReturnTextLeftAlignedWithWidth20() throws Exception {
+        String input = "This text should be left aligned ";
+
+        String output = Formatter.format(input);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+
+    @Test
+    public void shouldReturnTextRightAlignedWithWidth10() throws Exception {
+        String input = "This text should be right aligned ";
+
+        String output = Formatter.format(input);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+
+    @Test
+    public void shouldReturnTextRightAlignedWithWidth20() throws Exception {
+        String input = "This text should be right aligned ";
+
+        String output = Formatter.format(input);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+
+    @Test
+    public void shouldReturnTextCenterAlignedWithWidth10() throws Exception {
+        String input = "This text should be center aligned ";
+
+        String output = Formatter.format(input);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+
+    @Test
+    public void shouldReturnTextCenterAlignedWithWidth20() throws Exception {
+        String input = "This text should be center aligned ";
 
         String output = Formatter.format(input);
 
