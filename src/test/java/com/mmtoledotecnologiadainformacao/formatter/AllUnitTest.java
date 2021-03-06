4fee58a70f457762e04281f91b5bfe91e3e27cb4
marina-toledo <marina.mi.toledo@hotmail.com>
2021-03-06 19:27:15 +0100

    removing Spring Test dependency, adding Junit dependencies, creating class to run all tests

---
+++ src/test/java/com/mmtoledotecnologiadainformacao/formatter/AllUnitTest.java
@@ -0,0 +1,10 @@
+package com.mmtoledotecnologiadainformacao.formatter;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.mmtoledotecnologiadainformacao.formatter")
+public class AllUnitTest {
+}
