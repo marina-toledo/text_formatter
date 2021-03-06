2e869c6776467f8f6ac5eb80737b2d44fb2eb372
marina-toledo <marina.mi.toledo@hotmail.com>
2021-03-06 20:13:28 +0100

    calling method through standard in

--- src/main/java/com/mmtoledotecnologiadainformacao/formatter/FormatterApplication.java
+++ src/main/java/com/mmtoledotecnologiadainformacao/formatter/FormatterApplication.java
@@ -1,9 +1,28 @@
 package com.mmtoledotecnologiadainformacao.formatter;
 
+import com.mmtoledotecnologiadainformacao.formatter.service.TextAligner;
+
+import java.util.Scanner;
+
 public class FormatterApplication {
 
     public static void main(String[] args) {
+//        TODO: catch wrong typed inputs and adjust aligment call
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Application started...");
+
+        System.out.println("Input width: ");
+        int width = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Choose type of alignment (L-C-R): ");
+        String alignment = scanner.nextLine();
+
+        System.out.println("Input text: ");
+        String text = scanner.nextLine();
+
+        final String formattedText = TextAligner.format(text, TextAligner.Alignment.LEFT, width);
+        System.out.println(formattedText);
     }
 
 }
