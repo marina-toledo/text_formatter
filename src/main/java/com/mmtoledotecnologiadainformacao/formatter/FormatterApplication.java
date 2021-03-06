3e2796ed65312d72ce0b8a34a3de2cca9d023178
marina-toledo <marina.mi.toledo@hotmail.com>
2021-03-06 19:53:40 +0100

    removing spring dependency

--- src/main/java/com/mmtoledotecnologiadainformacao/formatter/FormatterApplication.java
+++ src/main/java/com/mmtoledotecnologiadainformacao/formatter/FormatterApplication.java
@@ -1,13 +1,9 @@
 package com.mmtoledotecnologiadainformacao.formatter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class FormatterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormatterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Application started...");
+    }
 
 }
