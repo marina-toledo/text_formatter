e1f8f8ab5dda944c41155eb1751ebdb11757625e
marina-toledo <marina.mi.toledo@hotmail.com>
2021-03-06 21:44:40 +0100

    defining test cases a little better

--- src/test/java/com/mmtoledotecnologiadainformacao/formatter/service/CenterAlignerTest.java
+++ src/test/java/com/mmtoledotecnologiadainformacao/formatter/service/CenterAlignerTest.java
@@ -6,18 +6,67 @@ import org.junit.jupiter.api.Test;
 import static com.mmtoledotecnologiadainformacao.formatter.service.TextAligner.Alignment.CENTER;
 
 public class CenterAlignerTest {
-//    TODO: add more test cases
-//    smaller than width
-//    string bigger than width
-//    empty string
-//    just space
-//    when words break exactly in the limits : start and end
-//    different sizes of space between words
-
-    TextAligner textAligner = new TextAligner();
+    int WIDTH = 20;
 
     @Test
-    public void shouldReturnTextCenterAlignedWithWidth10() throws Exception {
+    public void inputIsSmallerThanWidth() {
+//        TODO: Odd even sizes
+        String input = "0123456789";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("     0123456789     ", output);
+    }
+
+    @Test
+    public void inputIsBiggerThanWidth() {
+        String input = "012345678901234567890123456789";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("012345678901234567890\n123456789", output);
+    }
+
+    @Test
+    public void inputIsAnEmptyString() {
+        String input = "";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+    @Test
+    public void inputIsMadeOnlyOfSpaces() {
+        String input = "   ";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+    @Test
+    public void LineBreaksExactlyBetweenWords() {
+        String input = "01234567890123456789 0123456789";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+    @Test
+    public void inputHasContiguousSpacesBetweenWords() {
+        String input = "0123456789   0123456789";
+
+        String output = TextAligner.format(input, CENTER, WIDTH);
+
+        Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
+    }
+
+    // some random cases above
+
+    @Test
+    public void shouldReturnTextCenterAlignedWithWidth10() {
         String input = "This text should be center aligned ";
 
         String output = TextAligner.format(input, CENTER, 10);
@@ -25,9 +74,8 @@ public class CenterAlignerTest {
         Assertions.assertEquals("This text \nshould be \nleft \naligned ", output);
     }
 
-
     @Test
-    public void shouldReturnTextCenterAlignedWithWidth20() throws Exception {
+    public void shouldReturnTextCenterAlignedWithWidth20() {
         String input = "This text should be center aligned ";
 
         String output = TextAligner.format(input, CENTER, 20);
